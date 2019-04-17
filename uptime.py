26f34d9cd81cf0b8ede9a4dece043bbb494a61b8
Christopher Graham <chgraham@gmail.com>
2019-04-17 15:40:23 -0400

    basic functionality set in uptime.py

---
+++ uptime.py
@@ -0,0 +1,28 @@
+from urllib.request import urlopen
+from urllib.error import HTTPError, URLError
+import logging
+
+from time import time
+
+
+
+def check_page(url):
+    try:
+        response = urlopen(url, timeout=10).read().decode('utf-8')
+    except HTTPError as error:
+        logging.error("Data not retrived because %s\nURL: %s", error, url)
+    except URLError as error:
+        if isinstance(error.reason, socket.timeout):
+            logging.error('socket timed out - URL %s', url)
+        else:
+            logging.error('some other error happened')
+    else:
+        logging.info('Access successful')
+
+testurl = "http://www.google.ca"
+
+start_time = time()
+check_page(testurl)
+end_time = time()
+
+print(f'start1 to end = {end_time - start_time}')
