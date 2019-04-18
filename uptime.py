275213015b86b9b1f7aa772557a53f14b3aba546
Christopher Graham <chgraham@gmail.com>
2019-04-18 11:38:15 -0400

    added printout to uptime.py so you know what's going on

--- uptime.py
+++ uptime.py
@@ -1,9 +1,11 @@
 import time
+from datetime import datetime
 from monitor import UptimeMonitor
 
 json_url_file = 'urllist.json'
 monitor = UptimeMonitor(json_url_file)
 
 while True:
+    print(f'Site check at {datetime.today().strftime('%Y-%m-%d %H:%M:%S')}')
     monitor.check_all_sites()
     time.sleep(300)
