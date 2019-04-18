36b979bc3d623ba0e357668b01053d2b5965b0a6
Christopher Graham <chgraham@gmail.com>
2019-04-18 11:40:22 -0400

    fix quote mismatch in uptime.py

--- uptime.py
+++ uptime.py
@@ -6,6 +6,6 @@ json_url_file = 'urllist.json'
 monitor = UptimeMonitor(json_url_file)
 
 while True:
-    print(f'Site check at {datetime.today().strftime('%Y-%m-%d %H:%M:%S')}')
+    print(f"Site check at {datetime.today().strftime('%Y-%m-%d %H:%M:%S')}")
     monitor.check_all_sites()
     time.sleep(300)
